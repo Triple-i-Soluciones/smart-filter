7539a275b3e16eeecdfca4d066acaaa4d4b6e59e
Sergio Heynes <sergioheynes@machine.local>
2023-06-15 08:30:28 -0700

    Importaciones eliminadas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import {MatSelectModule} from '@angular/material/select';
-import { ReactiveFormsModule } from '@angular/forms';
-import {MatIconModule} from '@angular/material/icon';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SmartFilterModule } from './smart-filter/smart-filter.module';
 
 
